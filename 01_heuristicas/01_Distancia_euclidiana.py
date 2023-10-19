6ce4721dbb8c1d7c0aecebac08e0d1a8391b31ea
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-10-19 10:15:24 -0600

    Distancia Euclidiana

--- 01_heuristicas/01_Distancia_euclidiana.py
+++ 01_heuristicas/01_Distancia_euclidiana.py
@@ -1,25 +1,26 @@
-import heapq
-import math
+import heapq  # Importa la librería heapq para manejar la cola de prioridad.
+import math   # Importa la librería math para calcular la raíz cuadrada.
 
+# Define la función 'euclidean_distance' que calcula la distancia Euclidiana entre dos nodos 'node' y 'goal'.
 def euclidean_distance(node, goal):
-    # Heurística: Distancia Euclidiana desde 'node' hasta 'goal'
     return math.sqrt((node[0] - goal[0])**2 + (node[1] - goal[1])**2)
 
+# Define la función 'a_star_search' que realiza la búsqueda A* en un grafo.
 def a_star_search(graph, start, goal):
-    open_list = []  # Cola de prioridad para nodos abiertos
-    closed_set = set()  # Conjunto de nodos cerrados
-    came_from = {}  # Diccionario para rastrear el camino desde el nodo inicial
+    open_list = []       # Cola de prioridad para nodos abiertos.
+    closed_set = set()  # Conjunto de nodos cerrados.
+    came_from = {}      # Diccionario para rastrear el camino desde el nodo inicial.
     g_score = {node: float('inf') for node in graph}
     g_score[start] = 0
 
-    # Inicializar la cola de prioridad con el nodo de inicio y su costo estimado
+    # Inicializa la cola de prioridad con el nodo de inicio y su costo estimado.
     heapq.heappush(open_list, (0 + euclidean_distance(start, goal), start))
 
     while open_list:
-        _, current = heapq.heappop(open_list)
+        _, current = heapq.heappop(open_list)  # Obtiene el nodo con el menor costo estimado desde la cola.
 
         if current == goal:
-            # Reconstruir el camino desde el objetivo hasta el inicio
+            # Reconstruye el camino desde el objetivo hasta el inicio.
             path = []
             while current in came_from:
                 path.insert(0, current)
@@ -38,9 +39,9 @@ def a_star_search(graph, start, goal):
                 if neighbor not in [node[1] for node in open_list]:
                     heapq.heappush(open_list, (f_score, neighbor))
 
-    return None  # No se encontró un camino
+    return None  # No se encontró un camino.
 
-# Ejemplo de uso:
+# Ejemplo de uso: Definición del grafo y nodos de inicio y objetivo.
 graph = {
     (0, 0): {(0, 1): 1, (1, 0): 1},
     (0, 1): {(0, 0): 1, (1, 1): 1},
@@ -48,12 +49,13 @@ graph = {
     (1, 1): {(0, 1): 1, (1, 0): 1}
 }
 
-start_node = (0, 0)  # Nodo de inicio
-goal_node = (1, 1)   # Nodo objetivo
-result = a_star_search(graph, start_node, goal_node)  # Llamada a la función A* con heurística de distancia Euclidiana
+start_node = (0, 0)  # Nodo de inicio.
+goal_node = (1, 1)   # Nodo objetivo.
+
+result = a_star_search(graph, start_node, goal_node)  # Llamada a la función A* con heurística de distancia Euclidiana.
 
 if result:
     print("Camino encontrado:")
-    print(result)  # Imprimir la ruta encontrada
+    print(result)  # Imprimir la ruta encontrada.
 else:
     print("No se encontró un camino.")
